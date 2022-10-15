10b70f420a8ecc10930b2678ed5276a43da905b9
Christopher Kei <ckei@Christophers-MacBook-Air.local>
2022-10-15 15:22:10 -0400

    add github dots

--- components/contactView/dotPositions.js
+++ components/contactView/dotPositions.js
@@ -4,28 +4,93 @@ const dotPositions = {
     { left: 118, top: 118 },
   ],
   github: [
-    { left: 0, top: 176 },
-    { left: 118, top: 284 },
+    { left: -260, top: 208 }, // left before head
+    { left: -261, top: 203 },
+    { left: -260, top: 198 },
+    { left: -253, top: 195 },
+    { left: -258, top: 193 }, // tip of left ear
+    { left: -250, top: 198 },
+    { left: -248, top: 201 },
+    { left: -241, top: 200 },
+    { left: -236, top: 199 },
+    { left: -231, top: 198 }, // left top head midpoint
+    { left: -225, top: 198 }, // right top head midpoint
+    { left: -220, top: 199 },
+    { left: -215, top: 200 },
+    { left: -210, top: 201 },
+    { left: -208, top: 198 },
+    { left: -200, top: 193 }, // tip of right ear
+    { left: -205, top: 195 },
+    { left: -198, top: 198 },
+    { left: -197, top: 203 },
+    { left: -198, top: 208 }, // right before head
+
+    { left: -263, top: 213 }, // left start head
+    { left: -265, top: 218 },
+    { left: -266, top: 223 },
+    { left: -267, top: 228 },
+    { left: -267, top: 233 },
+    { left: -265, top: 238 },
+    { left: -262, top: 242 },
+    { left: -258, top: 246 },
+    { left: -253, top: 249 },
+    { left: -248, top: 250 },
+    { left: -243, top: 251 }, // left end head
+
+    { left: -195, top: 213 }, // right start head
+    { left: -193, top: 218 },
+    { left: -192, top: 223 },
+    { left: -191, top: 228 },
+    { left: -191, top: 232 },
+    { left: -193, top: 238 },
+    { left: -196, top: 242 },
+    { left: -200, top: 246 },
+    { left: -205, top: 249 },
+    { left: -210, top: 250 },
+    { left: -215, top: 251 }, // right end head
+
+    { left: -245, top: 256 }, // left start body
+    { left: -246, top: 261 },
+    { left: -247, top: 266 },
+    { left: -247, top: 271 },
+    { left: -247, top: 276 },
+    { left: -247, top: 281 },
+
+    { left: -213, top: 256 }, // right start body
+    { left: -212, top: 261 },
+    { left: -211, top: 266 },
+    { left: -211, top: 271 },
+    { left: -211, top: 276 },
+    { left: -211, top: 281 },
+
+    { left: -250, top: 267 }, // tail
+    { left: -255, top: 270 },
+    { left: -260, top: 271 },
+    { left: -265, top: 270 },
+    { left: -268, top: 267 },
+    { left: -271, top: 264 },
+    { left: -274, top: 262 },
+    { left: -278, top: 261 },
   ],
   linkedin: [
-    { left: 224, top: 176 },
-    { left: 342, top: 284 },
+    { left: -64, top: 176 },
+    { left: 54, top: 294 },
   ],
   email: [
-    { left: 448, top: 176 },
-    { left: 566, top: 284 },
+    { left: 160, top: 176 },
+    { left: 278, top: 294 },
   ],
   instagram: [
-    { left: 0, top: 400 },
-    { left: 118, top: 508 },
+    { left: -288, top: 400 },
+    { left: -170, top: 518 },
   ],
   tiktok: [
-    { left: 224, top: 400 },
-    { left: 342, top: 508 },
+    { left: -64, top: 400 },
+    { left: 54, top: 518 },
   ],
   coffee: [
-    { left: 448, top: 400 },
-    { left: 566, top: 508 },
+    { left: 160, top: 400 },
+    { left: 278, top: 518 },
   ],
 };
 
